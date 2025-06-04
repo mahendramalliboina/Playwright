01c958148c837fcee54be36adbf9f905f555982c
Mahendra Malleboina <mahendra.malleboinaina@surveysparrow.com>
2025-06-04 10:52:54 +0530

    updated playwright configfile and readmefile

--- playwright.config.js
+++ playwright.config.js
@@ -34,6 +34,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
@@ -45,15 +48,15 @@ export default defineConfig({
     },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,11 +1,15 @@
-// @ts-check
+
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
+  // Expect a title "to contain" a substring.here if we dont mention /../ it will expect exact tile 
   await expect(page).toHaveTitle(/Playwright/);
+  const pageTitle  =  await page.title();
+  console.log(pageTitle);
+  await expect(page).toHaveTitle(pageTitle);
+
 });
 
 test('get started link', async ({ page }) => {
